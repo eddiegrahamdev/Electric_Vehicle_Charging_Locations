7302a035ed60478ed1d9f470a65f71dae572ed71
MTLEGR <Eddie.Graham@sas.com>
2017-11-25 18:29:26 +0000

    Add markers to map

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,16 +10,73 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .controller('View1Ctrl', function($scope, $http, uiGmapGoogleMapApi) {
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 
-    $http.get('data/nationalChargePointRegistry.json').then(successCallback, errorCallback);
+    init();
+
+    function init() {
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8
+        };
+
+        centerMapOnGeolocation();
+
+        $http.get('data/nationalChargePointRegistry.json').then(successCallback, errorCallback);
+    }
+
+    function centerMapOnGeolocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(centerMap, geolocationPermissionDenied);
+        } else {
+            // TODO geolocation is not supported by this browser
+        }
+    }
+
+    function centerMap(position) {
+        $scope.map.center = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        };
+
+        $scope.$apply();
+    }
+
+    function geolocationPermissionDenied() {
+        // TODO
+    }
 
     function successCallback(response) {
-        console.log();
+        $scope.markers = [];
+
+        for (let i = 0; i < response.data.ChargeDevice.length; i++) {
+            let chargeDevice = response.data.ChargeDevice[i];
+
+            // We are only showing charging locations that are free from access restrictions and which
+            // do not require a subscription
+            if (!chargeDevice.AccessRestrictionFlag && !chargeDevice.SubscriptionRequiredFlag) {
+                $scope.markers.push(createMarkerObject(chargeDevice));
+            }
+        }
+    }
+
+    function createMarkerObject(chargeDevice) {
+        return {
+            id: chargeDevice.ChargeDeviceId,
+            coords: {
+                latitude: chargeDevice.ChargeDeviceLocation.Latitude,
+                longitude: chargeDevice.ChargeDeviceLocation.Longitude
+            },
+            options: {
+                title: chargeDevice.ChargeDeviceName
+            }
+        };
     }
 
     function errorCallback(response) {
-        console.log();
+        // TODO failed to GET data file
     }
 
     uiGmapGoogleMapApi.then(function(maps) {
